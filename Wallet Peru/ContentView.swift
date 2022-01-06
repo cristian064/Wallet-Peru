a2682aa26c6af0d5ec88fef67a3d3f1adf33d64b
cristian <cristian.ayala.064@gmail.com>
2022-01-05 19:23:11 -0500

    Initial Commit

---
+++ Wallet Peru/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wallet Peru
+//
+//  Created by cristian ayala on 5/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wallet Peru/Wallet_PeruApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wallet_PeruApp.swift
+//  Wallet Peru
+//
+//  Created by cristian ayala on 5/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Wallet_PeruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
